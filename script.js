7a706f379b6be98de2a61bbde8a69fd5b792f664
carlowta <trinident2009@gmail.com>
2024-11-26 20:51:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Responsive Navbar Toggle
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector(".navbar ul");
+
+    toggleButton.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+});
